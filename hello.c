76aba973bf21a07132c7bcc98f3110cf221027f3
tanxiaoo <xiaotan.scu@gmail.com>
2024-04-20 22:10:37 +0200

    Add tests for multiply function.

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+
+int multiply(int a,int b)
+{
+    return a * b;
+}
+
+int test()
+{
+    assert(multiply(2,2) == 4);
+    assert(multiply(-3,2) == 6);
+}
  
 int main() {
-    printf("Hello, World!");
+    printf("Yoyo,Little Ross!");
+    test()
     return 0;
 }
